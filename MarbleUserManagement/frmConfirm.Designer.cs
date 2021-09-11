5def7979443bf90513f9a324286e91f5fd3b18a6
miyazakimaiko <myzkmik19922@gmail.com>
2021-09-11 18:41:40 +0100

    Changed to be able to reuse the form for add, edit and delete form

---
+++ MarbleUserManagement/frmConfirm.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace GWUserManagement
+{
+    partial class frmConfirm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.buttonGoBack = new System.Windows.Forms.Button();
+            this.buttonConfirm = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelEmail = new System.Windows.Forms.Label();
+            this.labelPhone = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelImage = new System.Windows.Forms.Label();
+            this.labelAdministration = new System.Windows.Forms.Label();
+            this.labelGroup = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelMessage.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelMessage.ForeColor = System.Drawing.Color.Tomato;
+            this.labelMessage.Location = new System.Drawing.Point(0, 0);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(520, 70);
+            this.labelMessage.TabIndex = 1;
+            this.labelMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Email Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(62, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Phone Number:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(62, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Password:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(63, 339);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 15);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Administration:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(62, 259);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(43, 15);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Image:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(63, 386);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 15);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Group:";
+            // 
+            // buttonGoBack
+            // 
+            this.buttonGoBack.Location = new System.Drawing.Point(266, 437);
+            this.buttonGoBack.Name = "buttonGoBack";
+            this.buttonGoBack.Size = new System.Drawing.Size(97, 33);
+            this.buttonGoBack.TabIndex = 9;
+            this.buttonGoBack.Text = "Go Back";
+            this.buttonGoBack.UseVisualStyleBackColor = true;
+            this.buttonGoBack.Click += new System.EventHandler(this.buttonGoBack_Click);
+            // 
+            // buttonConfirm
+            // 
+            this.buttonConfirm.Location = new System.Drawing.Point(379, 437);
+            this.buttonConfirm.Name = "buttonConfirm";
+            this.buttonConfirm.Size = new System.Drawing.Size(97, 33);
+            this.buttonConfirm.TabIndex = 10;
+            this.buttonConfirm.Text = "Confirm";
+            this.buttonConfirm.UseVisualStyleBackColor = true;
+            this.buttonConfirm.Click += new System.EventHandler(this.buttonConfirm_Click);
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(222, 87);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(0, 15);
+            this.labelName.TabIndex = 11;
+            // 
+            // labelEmail
+            // 
+            this.labelEmail.AutoSize = true;
+            this.labelEmail.Location = new System.Drawing.Point(222, 131);
+            this.labelEmail.Name = "labelEmail";
+            this.labelEmail.Size = new System.Drawing.Size(0, 15);
+            this.labelEmail.TabIndex = 12;
+            // 
+            // labelPhone
+            // 
+            this.labelPhone.AutoSize = true;
+            this.labelPhone.Location = new System.Drawing.Point(222, 172);
+            this.labelPhone.Name = "labelPhone";
+            this.labelPhone.Size = new System.Drawing.Size(0, 15);
+            this.labelPhone.TabIndex = 13;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(222, 214);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(0, 15);
+            this.labelPassword.TabIndex = 14;
+            // 
+            // labelImage
+            // 
+            this.labelImage.Location = new System.Drawing.Point(222, 259);
+            this.labelImage.Name = "labelImage";
+            this.labelImage.Size = new System.Drawing.Size(253, 62);
+            this.labelImage.TabIndex = 15;
+            // 
+            // labelAdministration
+            // 
+            this.labelAdministration.AutoSize = true;
+            this.labelAdministration.Location = new System.Drawing.Point(223, 339);
+            this.labelAdministration.Name = "labelAdministration";
+            this.labelAdministration.Size = new System.Drawing.Size(0, 15);
+            this.labelAdministration.TabIndex = 16;
+            // 
+            // labelGroup
+            // 
+            this.labelGroup.AutoSize = true;
+            this.labelGroup.Location = new System.Drawing.Point(223, 386);
+            this.labelGroup.Name = "labelGroup";
+            this.labelGroup.Size = new System.Drawing.Size(0, 15);
+            this.labelGroup.TabIndex = 17;
+            // 
+            // frmConfirm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 504);
+            this.Controls.Add(this.labelGroup);
+            this.Controls.Add(this.labelAdministration);
+            this.Controls.Add(this.labelImage);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelPhone);
+            this.Controls.Add(this.labelEmail);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.buttonConfirm);
+            this.Controls.Add(this.buttonGoBack);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelMessage);
+            this.Name = "frmConfirm";
+            this.Text = "Confirm";
+            this.Load += new System.EventHandler(this.frmConfirm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button buttonGoBack;
+        private System.Windows.Forms.Button buttonConfirm;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelEmail;
+        private System.Windows.Forms.Label labelPhone;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelImage;
+        private System.Windows.Forms.Label labelAdministration;
+        private System.Windows.Forms.Label labelGroup;
+    }
+}
---
+++ MarbleUserManagement/frmConfirm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GWUserManagement
+{
+    public partial class frmConfirm : Form
+    {
+        private string formType;
+        private string email;
+        private User user;
+        public bool completed = false;
+
+        public frmConfirm(string formType, User user = null, string email = null)
+        {
+            InitializeComponent();
+
+            this.formType = formType;
+            this.email = email;
+            this.user = user;
+        }
+
+        private void frmConfirm_Load(object sender, EventArgs e)
+        {
+            labelName.Text = user.Name;
+            labelEmail.Text = user.Email;
+            labelPhone.Text = user.Phone;
+            labelPassword.Text = String.Concat(Enumerable.Repeat("*", user.Password.Length));
+            labelImage.Text = user.Image;
+            labelAdministration.Text = user.IsAdmin ? "Yes" : "No";
+            labelGroup.Text = user.Group;
+
+            if (formType == "add")
+            {
+                labelMessage.Text = "Please confirm the detail of the new user.";
+            }
+            else if (formType == "edit")
+            {
+                labelMessage.Text = "Please confirm the new detail of the user.";
+            }
+            else if (formType == "delete")
+            {
+                labelMessage.Text = "Are you sure to delete the user below?";
+            }
+        }
+
+        private void buttonGoBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonConfirm_Click(object sender, EventArgs e)
+        {
+            DBConnection connection = new DBConnection("localhost", "usermanagement", "root", "Maiko119922");
+            DBUsers dbUsers = new DBUsers();
+
+            connection.OpenConnection();
+
+            if (formType == "add")
+            {
+                dbUsers.Insert(connection, this.user);
+            }
+            else if (formType == "edit")
+            {
+                dbUsers.Update(connection, this.email, this.user);
+            }
+            else if (formType == "delete")
+            {
+                dbUsers.Delete(connection, this.user.Email);
+            }
+            connection.CloseConnection();
+            completed = true;
+            this.Close();
+        }
+
+        public bool isCompleted()
+        {
+            return completed;
+        }
+    }
+}
