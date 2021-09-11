4d1201e59301c31cf0c35311946c1f99f0c73cd3
miyazakimaiko <myzkmik19922@gmail.com>
2021-09-11 18:39:01 +0100

    Created separate form utility to enable reuse

---
+++ MarbleUserManagement/FormUtility.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GWUserManagement
+{
+    class FormUtility
+    {
+        public static bool findFilepath(TextBox textBoxImage)
+        {
+            int size = -1;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = openFileDialog1.FileName;
+                textBoxImage.Text = file;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+
+                    return true;
+                }
+                catch (IOException exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+            }
+            return false;
+        }
+
+        public static void clearErrorLabel(Control control)
+        {
+            if (control is Label)
+            {
+                Label lbl = (Label)control;
+                if (lbl.Name.StartsWith("labelError"))
+                    lbl.Text = String.Empty;
+
+            }
+            else
+                foreach (Control child in control.Controls)
+                {
+                    clearErrorLabel(child);
+                }
+        }
+    }
+}
